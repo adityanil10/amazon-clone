b0aa7d5136f5aac03738dbfd616c7738bc72e610
Aditya Anil Nair <adityanil10@gmail.com>
2022-11-22 12:44:32 +0530

    User Auth Begin

--- src/Login.js
+++ src/Login.js
@@ -1,7 +1,7 @@
 import React, {useState} from 'react'
 import './Login.css'
 import { Link } from 'react-router-dom';
-import { db } from './firebase';
+import { auth } from './firebase';
 
 
 function Login() {

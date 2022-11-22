7acfc524148b69ca03b13175fb6d937ccbb33ed7
Aditya Anil Nair <adityanil10@gmail.com>
2022-11-22 12:41:34 +0530

    User Auth begin

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,6 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: "AIzaSyBounp-DsuOhjHhkaAZw-87UIOx_RLU_CU",
@@ -8,3 +11,10 @@ const firebaseConfig = {
     appId: "1:890807511766:web:c091e93631d83987e6b777",
     measurementId: "G-CB9JFME9Q6"
   };
+
+  const firebaseApp = initializeApp(firebaseConfig);
+
+  const db = getFirestore(firebaseApp);
+  const auth = getAuth(firebaseApp);
+
+  export { db, auth };
